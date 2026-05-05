6676c14da7cc6764a12e5554fd40aad778ecca04
Anay Duggal <anayduggal22@gmail.com>
2026-05-05 13:37:20 +0530

    Time: 7 ms (80.47%), Space: 53 MB (65.28%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     bool dfs(vector<vector<int>>& graph, vector<int>& visited,
-             vector<int>& path_visited, int index) {
+             vector<int>& path_visited, vector<int>& safe, int index) {
+
+        // If already processed reusing the result
+        if (visited[index] == 1) {
+            return safe[index];
+        }
 
         visited[index] = 1;
         path_visited[index] = 1;
 
-        // Looping over connected nodes of index
+        // Loop over neighbors
         for (int i = 0; i < graph[index].size(); i++) {
+
             int neighboor = graph[index][i];
 
-            if (visited[neighboor] == 0) {
-                bool b = dfs(graph, visited, path_visited, neighboor);
-
-                // Cycle Detected
-                if (b == false) {
-                    return false;
-                }
+            if (visited[neighboor] == 1 
+            && path_visited[neighboor] == 1) {
+                return false; // cycle
             }
 
-            else if (visited[neighboor] == 1 && path_visited[neighboor] == 1) {
+            bool b = dfs(graph, visited, path_visited, safe, neighboor);
+            if (b == false) {
                 return false;
             }
         }
 
         // Backtracking
         path_visited[index] = 0;
+        safe[index] = 1;
         return true;
     }
 
@@ -35,12 +39,18 @@ public:
 
         vector<int> visited(m, 0);
         vector<int> path_visited(m, 0);
+        vector<int> safe(m, 0);
         vector<int> arr;
 
+        // Run DFS for all nodes
         for (int i = 0; i < m; i++) {
-            bool b = dfs(graph, visited, path_visited, i);
+            if (visited[i] == 0) {
+                dfs(graph, visited, path_visited, safe, i);
+            }
+        }
 
-            if (b == true) {
+        for (int i = 0; i < m; i++) {
+            if (safe[i] == 1) {
                 arr.push_back(i);
             }
         }
