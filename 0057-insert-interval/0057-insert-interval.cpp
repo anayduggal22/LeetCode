46000ef2d248af6c2fcad2f46e22dca3b9566e76
Anay Duggal <anayduggal22@gmail.com>
2026-06-05 08:59:31 +0530

    Time: 0 ms (100%), Space: 21.9 MB (14.48%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    static bool cmp(vector<int>& a, vector<int>& b) {
+        return a[0] < b[0];
+    }
+
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+
+        intervals.push_back(newInterval);
+
+        sort(intervals.begin(), intervals.end(), cmp);
+
+        vector<vector<int>> ans;
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for (int i = 1; i < intervals.size(); i++) {
+
+            if (intervals[i][0] <= end) {
+                end = max(end, intervals[i][1]);
+            }
+
+            else {
+                ans.push_back({start, end});
+
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+
+        ans.push_back({start, end});
+
+        return ans;
+    }
+};
