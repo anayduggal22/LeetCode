c2511702aef4e75cc8207b60ce92c3f20f9857c3
Anay Duggal <anayduggal22@gmail.com>
2026-06-22 14:27:39 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.04%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    int fact(int n, vector<int>& arr){
+
+        if(n <= 0){
+            return 1; // Only 1 way to climb 
+        }
+
+        if(n == 1){
+            return 1; // Only 1 way to climb
+        }
+
+        if(arr[n] != -1){
+            // Its value was already stored so return that
+            return arr[n];
+        }
+
+        int x = fact(n-1,arr);
+
+        int y = fact(n-2,arr);
+
+        return arr[n] = x + y;
+    }
+
+    int climbStairs(int n) {
+        
+        vector<int> arr(n+1,-1);
+
+        return fact(n,arr);
+    }
+};
