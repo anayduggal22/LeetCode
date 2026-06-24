7a61fa1f9799bd67b0c1e825fa4a77c501a8e688
Anay Duggal <anayduggal22@gmail.com>
2026-06-24 16:13:40 +0530

    Time: 0 ms (100%), Space: 11.8 MB (72.24%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
+    int fact(int m, int n, vector<vector<int>>& obstacleGrid,
+             vector<vector<int>>& dp) {
 
-        int fact(int m, int n,vector<vector<int>>& obstacleGrid,  vector<vector<int>>& dp) {
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
 
-        // Obstacle
-        if(m>=0 && n>= 0 && obstacleGrid[m][n] == 1){
-            return 0;
+                if (i == 0 && j == 0 && obstacleGrid[i][j] == 0) {
+                    dp[i][j] = 1;
+                }
+
+                else {
+                    int up = 0;
+                    int left = 0;
+
+                    if (obstacleGrid[i][j] == 1) {
+                        dp[i][j] = 0;
+                        continue;
+                    }
+
+                    if (i > 0)
+                        up = dp[i - 1][j];
+
+                    if (j > 0)
+                        left = dp[i][j - 1];
+
+                    dp[i][j] = up + left;
+                }
+            }
         }
 
-        else if (m == 0 && n == 0) {
-            return 1;
-        }
-
-        else if (m < 0 || n < 0) {
-            return 0;
-        }
-
-        else if (dp[m][n] != -1) {
-            return dp[m][n];
-        }
-
-        int up = fact(m - 1, n, obstacleGrid, dp);
-        int left = fact(m, n - 1, obstacleGrid,dp);
-
-        return dp[m][n] = up + left;
+        return dp[m][n];
     }
 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        
+
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
 
         vector<vector<int>> dp(m, vector<int>(n, -1));
 
-        return fact(m - 1, n - 1,obstacleGrid, dp);
+        return fact(m - 1, n - 1, obstacleGrid, dp);
     }
 };
