7d2dea0578d013349900741637ab9fe0b85eb654
Anay Duggal <anayduggal22@gmail.com>
2026-06-26 19:37:29 +0530

    Time: 2 ms (35.1%), Space: 16.4 MB (15.86%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
+    int fact(int n, int m, vector<vector<int>>& grid, vector<vector<int>>& dp) {
 
-    int fact(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp){
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
 
-        if(row == 0 && col == 0){
-            return grid[row][col];
-        } 
+                if (i == 0 && j == 0) {
+                    // Base Case
+                    dp[i][j] = grid[i][j];
+                }
 
-        else if(row < 0 || col < 0){
-            // Out of bound so return a big value
-            // So that it never gets chosen
-            return 1e9;
+                else {
+
+                    int up = 1e9;   // Should be default big
+                    int left = 1e9; // Should be default big
+
+                    if (i > 0) {
+                        up = grid[i][j] + dp[i - 1][j];
+                    }
+
+                    if (j > 0) {
+                        left = grid[i][j] + dp[i][j - 1];
+                    }
+
+                    dp[i][j] = min(up, left);
+                }
+            }
         }
 
-        else if(dp[row][col] != -1){
-            return dp[row][col];
-        }
-
-        int up = grid[row][col] + fact(row-1,col,grid,dp);
-        int left = grid[row][col] + fact(row,col-1,grid,dp);
-
-        return dp[row][col] = min(up,left);
+        return dp[n][m];
     }
 
     int minPathSum(vector<vector<int>>& grid) {
-        
-        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(),-1));
+
+        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), -1));
 
         int n = grid.size();
         int m = grid[0].size();
 
-        return fact(n-1,m-1,grid,dp);
+        return fact(n - 1, m - 1, grid, dp);
     }
 };
