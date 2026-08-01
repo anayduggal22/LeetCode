6d197528a339a070d9264900a31794301815af13
Anay Duggal <anayduggal22@gmail.com>
2026-08-01 21:23:44 +0530

    Time: 27 ms (66.58%), Space: 36.5 MB (56.64%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    int fact(int n, int amount, vector<vector<int>>& dp, vector<int>& coins){
+
+        if(n == 0){
+            if(amount % coins[0] == 0){
+                return 1; // We can use it m times to get to amount
+            }
+            else{
+                return 0;
+            }
+
+        }
+
+        if(dp[n][amount] != -1){
+            return dp[n][amount];
+        }
+
+        int nottake = 0 + fact(n-1, amount, dp, coins);
+
+        int take = 0;
+
+        if(coins[n] <= amount){
+            // There is a chance we can use this coin again, so n is not reduced
+            take =  fact(n, amount - coins[n], dp, coins);
+        }
+
+        return dp[n][amount] = take + nottake;
+    }
+
+    int change(int amount, vector<int>& coins) {
+        
+        vector<vector<int>>dp(coins.size(), vector<int>(amount + 1, -1));
+
+        int n = coins.size() -1;
+
+        return fact(n,amount,dp,coins);
+    }
+};
