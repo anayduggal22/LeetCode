e9fcb3b9ed65342cd4e4e2d9278d05d86677cf6e
Anay Duggal <anayduggal22@gmail.com>
2026-08-06 14:01:22 +0530

    Time: 23 ms (81.47%), Space: 27.5 MB (32.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,31 +4,39 @@ public:
              vector<vector<int>>& dp) {
 
         // Base Case
-        if (index1 < 0 || index2 < 0) {
-            return 0; // Out of Bound
+        for (int i = 0; i <= index1; i++) {
+            dp[i][0] = 0;
         }
 
-        if (dp[index1][index2] != -1) {
-            return dp[index1][index2];
+        for (int j = 0; j <= index2; j++) {
+            dp[0][j] = 0;
         }
 
-        // Common character found
-        if (text1[index1] == text2[index2]) {
-            return dp[index1][index2] = 1 + fact(index1 - 1, index2 - 1, text1, text2, dp);
+        for (int i = 1; i <= index1; i++) {
+            for (int j = 1; j <= index2; j++) {
+
+                // Common character found and here indexes are sub by 1
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+
+                // Common character NOT found
+                // So checking if going back to each string one-by-one can
+                // match another common or not
+                else {
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
         }
 
-        // Common character NOT found
-        // So checking if going back to each string one-by-one can
-        // match another common or not
-        else{
-            return dp[index1][index2] = max(fact(index1, index2 - 1, text1, text2, dp), fact(index1 - 1, index2, text1, text2, dp));
-        }
+        return dp[index1][index2];
     }
 
     int longestCommonSubsequence(string text1, string text2) {
 
-        int index1 = text1.length() -1;
-        int index2 = text2.length() -1;
+        // Not same as memoization, coz we are not sub 1
+        int index1 = text1.length();
+        int index2 = text2.length();
 
         vector<vector<int>> dp(index1 + 1, vector<int>(index2 + 1, -1));
 
