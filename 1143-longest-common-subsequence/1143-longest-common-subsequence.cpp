154cbf5aefb5e003f6e0d60dfd03c117821aef90
Anay Duggal <anayduggal22@gmail.com>
2026-08-06 14:04:11 +0530

    Time: 18 ms (87.72%), Space: 26.8 MB (80.14%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
-    int fact(int index1, int index2, const string& text1, const string& text2,
-             vector<vector<int>>& dp) {
+    int fact(int index1, int index2, const string& text1, const string& text2) {
+
+        vector<int> prev(index2 + 1, -1);
 
         // Base Case
-        for (int i = 0; i <= index1; i++) {
-            dp[i][0] = 0;
-        }
-
-        for (int j = 0; j <= index2; j++) {
-            dp[0][j] = 0;
+        for (int i = 0; i <= index2; i++) {
+            prev[i] = 0;
         }
 
         for (int i = 1; i <= index1; i++) {
+
+            vector<int> curr(index2 + 1, -1);
+            curr[0] = 0; // IMPORTANT
+
             for (int j = 1; j <= index2; j++) {
 
                 // Common character found and here indexes are sub by 1
                 if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    curr[j] = 1 + prev[j - 1];
                 }
 
                 // Common character NOT found
                 // So checking if going back to each string one-by-one can
                 // match another common or not
                 else {
-                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                    curr[j] = max(curr[j - 1], prev[j]);
                 }
             }
+
+            prev = curr;
         }
 
-        return dp[index1][index2];
+        return prev[index2];
     }
 
     int longestCommonSubsequence(string text1, string text2) {
@@ -38,8 +41,6 @@ public:
         int index1 = text1.length();
         int index2 = text2.length();
 
-        vector<vector<int>> dp(index1 + 1, vector<int>(index2 + 1, -1));
-
-        return fact(index1, index2, text1, text2, dp);
+        return fact(index1, index2, text1, text2);
     }
 };
