426f87b2421445b815946e0aa187e47ce3b7cba3
Anay Duggal <anayduggal22@gmail.com>
2026-08-09 22:38:08 +0530

    Time: 71 ms (57.06%), Space: 75.1 MB (79.33%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
 
-    int fact(vector<vector<int>>& dp, const string& s1, const string& s2, int i, int j){
+    int fact(const string& s1, const string& s2, int i, int j){
+
+        vector<int> prev(s1.length() + 1, -1);
 
         // Out of Bound
         for(int n = 0 ; n <= i ; n++){
-            dp[n][0] = 0;
-            dp[0][n] = 0;
+            prev[n] = 0;
         }
 
         for(int n = 1; n <= i; n++){
+            
+            vector<int> curr(s1.length() + 1, -1);
+            curr[0] = 0;
+
             for(int m = 1; m <= j; m++){
 
             if(s1[n-1] == s2[m-1]){
-                dp[n][m] = 1 + dp[n-1][m-1];
+                curr[m] = 1 + prev[m-1];
             }
             else{
-            dp[n][m] = max(dp[n-1][m], dp[n][m-1]);
+                curr[m] = max(prev[m], curr[m-1]);
             }
-            
+
             }
+
+            prev = curr;
         }
 
-        return dp[i][j];
+        return prev[j];
 
     }
 
@@ -35,8 +42,6 @@ public:
         string s2 = s;
         reverse(s2.begin(), s2.end());
 
-        vector<vector<int>> dp(s.length() + 1, vector<int>(s.length() + 1, -1));
-
         // Not subtracting by minus one as did in memoization
         // Due to base case of going negative cannot be done
         // In tabulation
@@ -44,6 +49,6 @@ public:
         int i = s.length();
         int j = s.length();
 
-        return fact(dp, s1, s2, i, j);
+        return fact(s1, s2, i, j);
     }
 };
