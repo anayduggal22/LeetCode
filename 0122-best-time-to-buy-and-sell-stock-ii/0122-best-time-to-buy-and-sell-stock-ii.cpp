9df24a8646460db522213ffcf09848a2f339006a
Anay Duggal <anayduggal22@gmail.com>
2026-08-24 13:56:47 +0530

    Time: 7 ms (39.33%), Space: 24.8 MB (23.76%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,52 +1,27 @@
 class Solution {
 public:
-
-    int fact(vector<int>& prices, vector<vector<int>>& dp, int index, int buy){
-
-        // Base Case
-        if(index == prices.size()){
-            // Out of bound so return 0;
-
-            return 0;
-        }
-
-        if(dp[index][buy] != -1){
-            return dp[index][buy];
-        }
-
-        int profit = 0;
-        // For buying, we will subtract that price
-        // For selling, we will add that price
-
-        if(buy == 0){
-            // Two cases to buy the stock at this index or go to next index without buying stock
-            // So after buying we will make buy = 1, because we want to sell it now
-            // And for going to next index, buy will be same
-
-            profit = max(-prices[index] + fact(prices,dp,index+1,1)
-            , 0 + fact(prices,dp,index +1, 0));
-        }
-
-        else if(buy == 1){
-            // Two cases to sell the stock at this index or go to next index without selling stock
-            // So for selling this we will make buy = 0, because we want to buy a new stock
-            // And for going to next index, buy will be same
-
-            profit = max(prices[index] + fact(prices,dp,index+1,0)
-            , 0 + fact(prices,dp,index +1, 1));
-        }
-
-        return dp[index][buy] = profit;
-    }
-
     int maxProfit(vector<int>& prices) {
-        
-        vector<vector<int>> dp(prices.size() + 1,vector<int>(2,-1));
+
+        vector<vector<int>> dp(prices.size() + 1, vector<int>(2, -1));
         // [prices.size()][2(buy OR sell)]
         // Buy = 0
         // Sell = 1
 
-        // Initially we want to buy a stock
-        return fact(prices, dp, 0, 0);
+        // BASE CASE
+        dp[prices.size()][0] = dp[prices.size()][1] = 0;
+
+        for (int i = prices.size() - 1; i >= 0; i--) {
+
+            // Buying state
+            dp[i][0] = max(-prices[i] + dp[i + 1][1],
+                           0 + dp[i + 1][0]);
+
+            // Selling state
+            dp[i][1] = max(prices[i] + dp[i + 1][0],
+                           0 + dp[i + 1][1]);
+        }
+
+        // Initially we are allowed to buy
+        return dp[0][0];
     }
 };
