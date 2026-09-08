30ab54154d92ff5869c289db2bf8ef0d02d5e35e
Anay Duggal <anayduggal22@gmail.com>
2026-09-08 14:06:52 +0530

    Time: 0 ms (100%), Space: 15 MB (85.8%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -4,28 +4,30 @@ public:
 
         // Base Case = Direct initialize dp grid with 0
 
-        vector<vector<int>> dp(prices.size() + 2,vector<int>(2,0));
-        // [prices.size()][2(buy OR sell)]
-        // Buy = 0
-        // Sell = 1
-        // size + 2 done as we will access i + 2
+        vector<int> curr(2,0);
+        vector<int> ahead1(2,0);
+        vector<int> ahead2(2,0);
 
 
-        //dp[i] depends on future states. So those future states must already be calculated.So we need to go backwards
+        //So those future states must already be calculated.So we need to go backwards
 
         for (int i = prices.size() - 1; i >= 0; i--) {
 
             // Buying state
-            dp[i][0] = max(-prices[i] + dp[i+1][1],
-                           0 + dp[i+1][0]);
+            curr[0] = max(-prices[i] + ahead1[1],
+                           0 + ahead1[0]);
 
             // Selling state
-            dp[i][1] = max(prices[i] + dp[i+2][0],
-                           0 + dp[i+1][1]);
+            curr[1] = max(prices[i] + ahead2[0],
+                           0 + ahead1[1]);
+
+            // IMPORTANT
+            ahead2 = ahead1;
+            ahead1 = curr;
 
         }
 
         //Returning Buy
-        return dp[0][0];
+        return ahead1[0];
     }
 };
