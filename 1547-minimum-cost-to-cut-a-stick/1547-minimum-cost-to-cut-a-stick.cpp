7e3e442d3213117d913b9c7acabbd8877970270b
Anay Duggal <anayduggal22@gmail.com>
2026-09-22 10:45:53 +0530

    Time: 16 ms (88.32%), Space: 13.9 MB (80.77%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
-
-    int fact(int i, int j, vector<int>& cuts, vector<vector<int>>& dp){
-
-        // Base Case
-        if(i > j){
-            return 0;
-        }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        int m = INT_MAX;
-
-        for(int k = i ; k <= j; k++){
-
-            int cost = (cuts[j+1] - cuts[i-1]) + 
-            fact(i,k-1,cuts,dp) + fact(k+1,j,cuts, dp);
-
-            m = min(m,cost);
-        }
-
-
-        return dp[i][j] = m;
-    }
-
     int minCost(int n, vector<int>& cuts) {
         int c = cuts.size();
 
         cuts.push_back(n);
-        cuts.insert(cuts.begin(),0);
+        cuts.insert(cuts.begin(), 0);
 
         // IMPORTANT
         sort(cuts.begin(), cuts.end());
 
-        vector<vector<int>> dp(c + 1, vector<int>(c + 1, -1));
+        // INITIALIZE TO 0
+        vector<vector<int>> dp(c + 2, vector<int>(c + 2, 0));
 
-        // i, j, cuts, dp
-        return fact(1,c,cuts,dp);
+        // Doing reverse of memoization
+        // i=1 to c -> c to 1
+        // j = c to 1 -> 1 to c
+
+        for (int i = c; i >= 1; i--) {
+            for (int j = 1; j <= c; j++) {
+
+                // Base Case
+                if (i > j) {
+                    continue;
+                }
+
+                int m = INT_MAX;
+
+                for (int k = i; k <= j; k++) {
+
+                    int cost = (cuts[j + 1] - cuts[i - 1]) 
+                    + dp[i][k - 1] 
+                    + dp[k + 1][j];
+
+                    m = min(m, cost);
+                }
+
+                dp[i][j] = m;
+            }
+        }
+
+        return dp[1][c];
     }
 };
