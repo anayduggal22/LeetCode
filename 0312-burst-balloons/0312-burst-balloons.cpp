d5f9ba6208c39470cde627ead83acc82af11a00e
Anay Duggal <anayduggal22@gmail.com>
2026-09-23 15:29:38 +0530

    Time: 101 ms (71.34%), Space: 14 MB (30.12%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -13,17 +13,17 @@ public:
         vector<vector<int>> dp(nums.size() + 1,
                                vector<int>(nums.size() + 1, 0));
 
-        int n = nums.size() - 2;
-
         // We will do opposite of Recursive Method
         // i => 1 to n => now n to 1
         // j => n to 1 => now 1 to n
 
         for (int i = nums.size() - 2; i >= 1; i--) {
+            for (int j = 1; j <= nums.size() -2 ; j++) {
 
-            // j will start from i as we always want j >= i
-
-            for (int j = i; j <= nums.size() - 2; j++) {
+                // Base Case
+                if (i > j) {
+                    continue;
+                }
 
                 int m = INT_MIN;
 
